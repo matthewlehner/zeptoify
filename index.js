a3a634e45ec2ab7913b5d564a74c705606270462
Matthew Lehner <matthewlehner@gmail.com>
2012-06-12 23:33:44 -0700

    proper fx filename

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = require('./lib/zepto');
 module.exports = require('./lib/ajax');
 module.exports = require('./lib/event');
 module.exports = require('./lib/form');
-module.exports = require('./lib/effects');
+module.exports = require('./lib/fx');
 module.exports = require('./lib/polyfill');
 module.exports = require('./lib/detect');
