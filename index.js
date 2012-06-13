0ee6a869c49c27d0187e14e272069fb34de407e7
Matthew Lehner <matthewlehner@gmail.com>
2012-06-12 23:22:16 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = require('./lib/zepto');
+module.exports = require('./lib/core');
+module.exports = require('./lib/ajax');
+module.exports = require('./lib/event');
+module.exports = require('./lib/form');
+module.exports = require('./lib/effects');
+module.exports = require('./lib/polyfill');
+module.exports = require('./lib/detect');
